3ae1e1ed7e8d8c4438355254e40a4aede4e917fc
Sergey Yelin <elinsn@gmail.com>
2019-10-10 19:29:12 +0300

    Add git

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,1 +1,2 @@
 echo 'PORTDIR_OVERLAY="/vagrant/gentoo-overlay ${PORTDIR_OVERLAY}"' >> /etc/portage/make.conf
+USE="-cgi -gpg -highlight -mediawiki -mediawiki-experimental -pcre-jit -perforce -ppcsha1 -subversion -tk -webdav" emerge dev-vcs/git
